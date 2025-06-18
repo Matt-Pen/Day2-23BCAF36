16fbf54c3882e3152b0dc71581f606e82db719d4
sona.kv <sonabalachandran13@gmail.com>
2025-06-18 11:57:40 +0530

    banking commit 3

--- src/main/java/Banking/Main.java
+++ src/main/java/Banking/Main.java
@@ -1,15 +1,43 @@
 package Banking;
 
 import com.mongodb.client.MongoDatabase;
+import org.example.Book;
+import org.example.FictionBook;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
+        Scanner scan= new Scanner(System.in);
         Account acnt= new Account();
-//        acnt.createaccount();
-//        acnt.deposit();
-//        acnt.withdrawal();
-        acnt.checkbal();
+
+        while(true){
+            System.out.println("Enter your choice:");
+            System.out.println("1. Create a New Account.");
+            System.out.println("2. Deposit into your account.");
+            System.out.println("3. Withdraw from your account.");
+            System.out.println("4. Check your account balance.");
+            int ch;
+            ch=scan.nextInt();
+            switch(ch){
+                case 1:
+                    acnt.createaccount();
+                    break;
+                case 2:
+                    acnt.deposit();
+                    break;
+                case 3:
+                    acnt.withdrawal();
+                    break;
+                case 4:
+                    acnt.checkbal();
+                    break;
+
+
+            }
+        }
+//
 
     }
 
